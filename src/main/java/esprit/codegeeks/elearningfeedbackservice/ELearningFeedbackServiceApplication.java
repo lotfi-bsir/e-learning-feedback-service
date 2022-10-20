0c76603531aa5755d3350ad1bf34a7a3f4c26bf9
lotfi_bsir <lotfi.bsir@esprit.tn>
2022-10-20 22:06:43 +0100

    create project

---
+++ src/main/java/esprit/codegeeks/elearningfeedbackservice/ELearningFeedbackServiceApplication.java
@@ -0,0 +1,13 @@
+package esprit.codegeeks.elearningfeedbackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningFeedbackServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ELearningFeedbackServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/esprit/codegeeks/elearningfeedbackservice/ELearningFeedbackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package esprit.codegeeks.elearningfeedbackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningFeedbackServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
